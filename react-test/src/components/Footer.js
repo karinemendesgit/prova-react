8ab920b7c6eb57dfe8d202fead554861611d907a
Karine Mendes <karinemendessantos@hotmail.com>
2021-12-08 12:33:00 -0300

    Feat: created footer

---
+++ react-test/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h3>Copyright 2021 Luby Software</h3>
+    </div>
+  );
+}
+
+export default Footer;
