a940d953a7eef1e6450787277517ef44deff34e3
Karine Mendes <karinemendessantos@hotmail.com>
2021-12-09 12:00:26 -0300

    Feat: add missing pages on routes

--- react-test/src/Routes.tsx
+++ react-test/src/Routes.tsx
@@ -1,11 +1,12 @@
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import ResetPassword from "./pages/ResetPassword";
+import Home from "./pages/Home";
+import NewBet from "./pages/NewBet";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 
-import React from "react";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function Routes(): JSX.Element {
   return (
@@ -17,7 +18,8 @@ function Routes(): JSX.Element {
           <Route path="/register" exact component={Registration}/>
           <Route path="/reset-password" exact component={ResetPassword}/>
         </Sidebar>
-        
+          <Route path="/home" exact component={Home} />
+          <Route path="/new-bet" exact component={NewBet} />
       </Footer>
     </Switch>
     </Router>
