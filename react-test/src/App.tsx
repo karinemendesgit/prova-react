d156c5801511165e9bf89c224fdea47f52ad1257
Karine Mendes <karinemendessantos@hotmail.com>
2021-12-09 16:48:38 -0300

    Feat: add Routes and react-router-dom in app

--- react-test/src/App.tsx
+++ react-test/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
 import Routes from './Routes';
 
 function App() {
   return (
     <div className="App">
       <Routes />
+      <Footer />
     </div>
   );
 }
