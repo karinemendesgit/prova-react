0c56f1e1a0cb01708c56ae781c941831d7ade058
Karine Mendes <karinemendessantos@hotmail.com>
2021-12-09 16:48:16 -0300

    Feat: trying to correct a bug in routes

--- react-test/src/Routes.tsx
+++ react-test/src/Routes.tsx
@@ -4,15 +4,13 @@ import ResetPassword from "./pages/ResetPassword";
 import Home from "./pages/Home";
 import NewBet from "./pages/NewBet";
 import Sidebar from "./components/Sidebar";
-import Footer from "./components/Footer";
 
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { Route, Switch} from "react-router-dom";
 
 function Routes(): JSX.Element {
   return (
-    <Router>
+    <>
       <Switch>
-      <Footer>
         <Sidebar>
           <Route path="/" exact component={Login}/>
           <Route path="/register" exact component={Registration}/>
@@ -20,11 +18,8 @@ function Routes(): JSX.Element {
         </Sidebar>
           <Route path="/home" exact component={Home} />
           <Route path="/new-bet" exact component={NewBet} />
-      </Footer>
-    </Switch>
-    </Router>
-    
-
+      </Switch>
+    </>
   )
 }
 
