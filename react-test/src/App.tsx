6b51a5e584d5a60d091eae1ce9a058b43bbfecc1
Karine Mendes <karinemendessantos@hotmail.com>
2021-12-15 20:46:13 -0300

    Feat: incorporated routes file

--- react-test/src/App.tsx
+++ react-test/src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css';
 import Footer from './components/Footer';
-import Routing from './Routing';
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import ResetPassword from "./pages/ResetPassword";
+import Home from "./pages/Home";
+import NewBet from "./pages/NewBet";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routing />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Registration/>}/>
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/new-bet" element={<NewBet/>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
