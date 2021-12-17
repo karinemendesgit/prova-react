4398098c7f265e6e611a4c71cb441d8df6d2c0fd
Karine Mendes <karinemendessantos@hotmail.com>
2021-12-17 16:51:08 -0300

    Feat: use redux to authenticated login page

--- react-test/src/components/Header.tsx
+++ react-test/src/components/Header.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import classes from "../styles/header.module.css";
+import { authActions } from "../store/auth";
 
 const Header: React.FC = () => {
   const location = useLocation();
   const path = location.pathname;
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
+
   return (
     <header className={classes.header}>
       <div className={classes.head1}>
@@ -18,7 +26,7 @@ const Header: React.FC = () => {
       <div className={classes.head2}>
         <h1>Account</h1>
         <div className={classes.logout}>
-          <Link to="/">
+          <Link onClick={logoutHandler} to="/">
             <h1>Log out</h1> 
             <FontAwesomeIcon icon={faArrowRight}/>
           </Link>
--- react-test/src/pages/Login.tsx
+++ react-test/src/pages/Login.tsx
@@ -1,31 +1,41 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import Sidebar from "../components/Sidebar";
 import classes from "../styles/login.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { authActions } from "../store/auth";
 
 const Login: React.FC = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
-  const [isLogin, setIsLogin] = useState(true);
   const location = useLocation();
   const dispatch = useDispatch();
+
+  const loginHandler = (event:any) => {
+    event.preventDefault();
+    dispatch(authActions.login());
+  }
   return (
     <div className={classes.container}>
       <Sidebar/>
       <div className={classes.login}>
         <h3>Authentication</h3>
         <div className={classes.containerLogin}>
-          <div>
+          <form onSubmit={loginHandler}>
             <div className={classes.inputLogin}>
               <input type="email" name="" id="" placeholder="Email" ref={emailRef} required/>
             </div>
             <div className={classes.inputLogin}>
-              <input type="password" name="" id="" placeholder="Password" ref={passwordRef} required/>
+              <input 
+              type="password" 
+              placeholder="Password"
+              //value={} 
+              ref={passwordRef} 
+              required/>
             </div>
-          </div>
+          </form>
           <Link to="/reset-password">
             <p className={classes.questionLogin}>I forget my password</p>
           </Link>
